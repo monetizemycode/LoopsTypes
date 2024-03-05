8c7c1f972a85a471c11926bded302819104fc897
Avinash <159287552+monetizemycode@users.noreply.github.com>
2024-03-05 22:40:17 +0530

    Syntax of Do while loop
    How to use do while loop

---
+++ SyntaxOfDoWhileLoop.java
@@ -0,0 +1,37 @@
+// Syntac of Do While Loop
+
+// Declaration of Class name as DoWhileLoopExample
+
+class SyntaxOfDoWhileLoop
+{
+  public static void main (String [] args)
+  {
+    int a = 1;
+
+    do
+      {
+      System.out.println(a);
+        a++;
+        // a++ is nothing but a=a+1
+  } 
+      while (a<=10);
+      }
+}
+
+/*
+OutPut
+  1: Compile Time: Success
+  2: Run Time: Success:
+  
+  1
+  2
+  3
+  4
+  5
+  6
+  7
+  8
+  9
+  10
+
+*/
